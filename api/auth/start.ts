47a2f7d1db12dcc6267ac5e4c85fbe273d5ee573
Jared Clayborn <jared.the.engineer1@gmail.com>
2026-01-27 22:04:15 -0700

    fix redirect uri typo - env var should only be base

--- api/auth/start.ts
+++ api/auth/start.ts
@@ -3,6 +3,12 @@ import { generatePKCE } from "../../lib/pkce";
 import { encodeState } from "../../lib/state";
 
 export const handler = (req: VercelRequest, res: VercelResponse) => {
+  console.log("AUTH START");
+  console.log({
+    clientId: process.env.DEXCOM_CLIENT_ID,
+    redirectUri: process.env.DEXCOM_REDIRECT_URI,
+    authUrl: process.env.DEXCOM_AUTH_URL,
+  });
   const { challenge, verifier } = generatePKCE();
 
   const state = encodeState({
@@ -10,15 +16,22 @@ export const handler = (req: VercelRequest, res: VercelResponse) => {
     ts: Date.now(),
   });
 
+  console.log("STATE: ", state);
+
   const params = new URLSearchParams({
     client_id: process.env.DEXCOM_CLIENT_ID!,
-    redirect_uri: `${process.env.BASE_URL}/api/auth/callback`,
+    redirect_uri: `${process.env.DEXCOM_REDIRECT_URI}/api/auth/callback`,
     response_type: "code",
     scope: "egvs offline_access",
     state,
     code_challenge: challenge,
     code_challenge_method: "S256",
   });
+
+  console.log(
+    "REDIRECTING TO:",
+    `${process.env.DEXCOM_AUTH_URL}?${params.toString()}`,
+  );
 
   res.redirect(`${process.env.DEXCOM_AUTH_URL}?${params.toString()}`);
 };
