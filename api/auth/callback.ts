5c70e34c4444abdd84c7907366caae08e468488f
Jared Clayborn <jared.the.engineer1@gmail.com>
2026-02-01 13:58:33 -0700

    add logs

--- api/auth/callback.ts
+++ api/auth/callback.ts
@@ -1,30 +1,31 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import axios from "axios";
-import { decodeState } from "../../lib/state";
-import { kv } from "@vercel/kv";
-import { DexcomSession } from "../../types";
-import { FIVE_MINUTES, THIRTY_DAYS_SECONDS } from "../../constants";
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import axios from 'axios';
+import { decodeState } from '../../lib/state';
+import { kv } from '@vercel/kv';
+import { DexcomSession } from '../../types';
+import { FIVE_MINUTES, THIRTY_DAYS_SECONDS } from '../../constants';
 
 export const handler = async (req: VercelRequest, res: VercelResponse) => {
   const { code, state } = req.query;
 
   if (!code || !state) {
-    return res.status(400).send("Missing code or state");
+    return res.status(400).send('Missing code or state');
   }
 
   const { verifier, ts } = decodeState<{ verifier: string; ts: number }>(
-    state as string,
+    state as string
   );
 
   if (Date.now() - ts > FIVE_MINUTES) {
-    return res.status(400).send("State expired");
+    return res.status(400).send('State expired');
   }
 
   try {
+    console.log('redirect uri: ', process.env.DEXCOM_REDIRECT_URI);
     const tokenRes = await axios.post(
       process.env.DEXCOM_TOKEN_URL!,
       new URLSearchParams({
-        grant_type: "authorization_code",
+        grant_type: 'authorization_code',
         client_id: process.env.DEXCOM_CLIENT_ID!,
         client_secret: process.env.DEXCOM_CLIENT_SECRET!,
         code: code as string,
@@ -32,10 +33,12 @@ export const handler = async (req: VercelRequest, res: VercelResponse) => {
         code_verifier: verifier,
       }),
       {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      },
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      }
     );
 
+    const text = await res.getMaxListeners();
+    console.error('DEXCOM TOKEN RESPONSE:', text);
     const sessionId = crypto.randomUUID();
     const session: DexcomSession = {
       accessToken: tokenRes.data.access_token,
@@ -51,10 +54,10 @@ export const handler = async (req: VercelRequest, res: VercelResponse) => {
 
     res.redirect(redirect);
   } catch (err: any) {
-    console.error("TOKEN ERROR STATUS:", err.response?.status);
-    console.error("TOKEN ERROR DATA:", err.response?.data);
-    console.error("TOKEN ERROR HEADERS:", err.response?.headers);
-    res.status(500).send("Token exchange failed");
+    console.error('TOKEN ERROR STATUS:', err.response?.status);
+    console.error('TOKEN ERROR DATA:', err.response?.data);
+    console.error('TOKEN ERROR HEADERS:', err.response?.headers);
+    res.status(500).send('Token exchange failed');
   }
 };
 
